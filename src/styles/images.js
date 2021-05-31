a8b93b426233a398c0b5b532d7a0546271038950
Ji Yeon Park <jyp933@gmail.com>
2021-05-31 12:37:37 +0900

    이미지파일 보관

---
+++ src/styles/images.js
@@ -0,0 +1,13 @@
+import CheckBoxOutline from '../../assets/icons/checkbox_outline.png';
+import CheckBox from '../../assets/icons/checkbox.png';
+import Delete from '../../assets/icons/delete.png';
+import Edit from '../../assets/icons/edit.png';
+import cancelEdit from '../../assets/icons/cancelEdit.png';
+
+export const images = {
+  unCompleted: CheckBoxOutline,
+  completed: CheckBox,
+  delete: Delete,
+  edit: Edit,
+  cancel: cancelEdit,
+};
